9ad8a4ea2c02f9b4367bd5f7fdbcb14afd4145e8
Furkan <furkan_blkc@hotmail.com>
2023-02-26 15:13:58 +0300

    test created

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,56 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render, screen, fireEvent } from "@testing-library/react";
+import emojiList from "./emojiList.json";
 import App from "./App";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+describe("Emoji Search Tests", () => {
+  let header, emoji, input;
+  let filterList;
+
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  /* Baslik testi */
+  test("header test", () => {
+    header = screen.getByText(/Emoji Search/i);
+    expect(header).toBeInTheDocument;
+    const images = screen.getAllByRole("img");
+    expect(images[0]);
+    expect(images[1]);
+  });
+
+  /* Emoji listesi testi */
+  test("emoji list control", () => {
+    emoji = emojiList.slice(0, 19);
+
+    emoji.map((item) => {
+      expect(screen.getByText(item.title)).toBeInTheDocument;
+    });
+  });
+
+  /* Emoji listesi filtreleme testi */
+  test("emoji list filter", () => {
+    input = screen.getByRole("textbox"); // input'a ulaşalım
+    const filter = "smile cat";
+    filterList = emojiList.filter(
+      (it) =>
+        it.keywords.toLowerCase().match(filter) ||
+        it.title.toLowerCase().match(filter)
+    );
+    fireEvent.change(input, { target: { value: filter } });
+    expect(screen.getAllByText(/cat/i)).toHaveLength(2);
+  });
+
+  /* Emoji listesindeki emojiye tıklama testi */
+  test("after click emoji", async () => {
+    const click = screen.getByText("100");
+    expect(
+      click.parentElement.getAttribute("data-clipboard-text").length
+    ).toBeGreaterThan(0);
+    console.log(click.parentElement.getAttribute("data-clipboard-text"));
+    expect(click.parentElement.getAttribute("data-clipboard-text")).toMatch(
+      "💯"
+    );
+  });
 });
